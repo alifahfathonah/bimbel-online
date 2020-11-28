4a46248ea6a693a363709dfd8cf7c19d5f633b17
novaardiansyah1 <novaardiansyah817@gmail.com>
2020-11-28 12:35:49 +0700

    .gitignore

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
 	/**
 	 * @var string
 	 */
-	public $fromEmail;
+	public $fromEmail = '';
 
 	/**
 	 * @var string
 	 */
-	public $fromName;
+	public $fromName = 'Bimbel Online (Bimbline)';
 
 	/**
 	 * @var string
